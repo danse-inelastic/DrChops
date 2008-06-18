466b9c6ed28e679e8475cf502c8d058408c8ccbd
Jiao Lin <linjiao@caltech.edu>
2008-06-18 13:19:00 +0000

    added test for events2mslicefiles parallel reduction

---
+++ arcseventdata/tests/applications/test_events2mslicefiles_p.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+rm -f IpdpE.h5 mslice.spe mslice.phx
+events2mslicefiles.py -o IpdpE.h5 --mslice-prefix=mslice --mpirun.nodes=12 --E -50,50,1 -x ARCS.xml -I 98.58  --journal.warning.Event2d=off --journal.info.mpirun  --journal.info.mpi ARCS_297/ARCS_297_neutron_event.dat
+
