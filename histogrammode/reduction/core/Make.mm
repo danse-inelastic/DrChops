30694ffa23b99f299b66cad301a26bf0f55bc870
Jiao Lin <linjiao@caltech.edu>
2008-06-21 01:42:10 +0000

    reduction.core Make.mm fixed. was damaged in revision 60

--- histogrammode/reduction/core/Make.mm
+++ histogrammode/reduction/core/Make.mm
@@ -34,6 +34,38 @@ update: clean
 # export
 
 EXPORT_PYTHON_MODULES = \
+	Abstract1DFunctionFitter.py 	\
+	AbstractTof2E.py 	\
+	AbstractIncidentEnergySolver.py \
+	AbstractNormalizationCalculator.py \
+	AbstractTimeIndependentBackgroundRemover.py \
+	AbstractIdpt2Spe.py	\
+	AbstractIdpt2Sqe.py	\
+	ApplyMaskToHistogram.py \
+	DifferentialEvolution1DFunctionFitter.py \
+	DetEfficiency.py \
+	HistogramCombiner.py \
+	IdptExtractor.py	\
+	Idpt2Spe_a.py		\
+	Idpt2Sqe.py		\
+	IncidentEnergySolver_UseMonitors.py \
+	IncidentEnergySolver_UseElasticPeaks.py \
+	LoopUtils.py \
+	MaskFromVanadiumData.py \
+	NormalizationConstantFromMonitorData.py \
+	NormalizationConstantFromIntegratedModeratorCurrent.py \
+	TimeIndependentBackgroundRemover.py \
+	TimeIndependentBackgroundRemover_AverageOverAllDetectors.py \
+	ParallelComponent.py		\
+	Spe2Sqe.py		\
+	Tof2E.py 		\
+	VDataProcessor.py	\
+	VanadiumTransmissionCalculator.py\
+	getDetectorInfo.py 	\
+	getPixelInfo.py 	\
+	mask.py          	\
+	numeric_functors.py 	\
+	utils.py 		\
 	__init__.py		\
 
 
