bb78be51bdc4d522e9c733dd8c9674d5a8b2a41b
Jiao Lin <linjiao@caltech.edu>
2009-11-25 14:46:39 +0000

    arcseventdata/__init__.py:write_mslice_files: new keyword argument "mask_p"

--- arcseventdata/arcseventdata/__init__.py
+++ arcseventdata/arcseventdata/__init__.py
@@ -185,10 +185,10 @@ def e2Itof(
 
 #from pixelpositions2angles import pixelpositions2angles
 
-def write_mslice_files( IpE, phi_p, psi_p, dphi_p, dpsi_p, spefile, phxfile ):
+def write_mslice_files( IpE, phi_p, psi_p, dphi_p, dpsi_p, spefile, phxfile, mask_p=None):
     from mslice_spe_writer import writer
-    writer.write_spe( IpE, phi_p, spefile )
-    writer.write_phx( phi_p, psi_p, dphi_p, dpsi_p, phxfile )
+    writer.write_spe( IpE, phi_p, spefile, mask_p=mask_p)
+    writer.write_phx( phi_p, psi_p, dphi_p, dpsi_p, phxfile, mask_p=mask_p)
     return
 
 
